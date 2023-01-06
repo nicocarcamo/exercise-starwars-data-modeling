054d1837873b53c0916dc9b9d4ac77ca1ec40680
nicocarcamo <nicolascarcamonaccarino@gmail.com>
2023-01-06 15:51:47 +0000

    base uml diagram

--- src/models.py
+++ src/models.py
@@ -1,3 +1,4 @@
+
 import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
@@ -8,26 +9,47 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    favorites = relationship('Favorites', back_populates='user')
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Character(Base):
+    __tablename__ = 'character'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String, nullable=False)
+    favorites = relationship('Favorites', back_populates='character')
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    favorites = relationship('Favorites', back_populates='planet')
 
-## Draw from SQLAlchemy base
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    favorites = relationship('Favorites', back_populates='vehicle')
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    starship_id = Column(Integer, ForeignKey('vehicle.id'))
+    favorite_type = Column(String, nullable=False)
+    user = relationship('User', back_populates='favorites')
+    character = relationship('Character', back_populates='favorites', uselist=False)
+    planet = relationship('Planet', back_populates='favorites', uselist=False)
+    vehicle = relationship('Vehicle', back_populates='favorites', uselist=False)
+
 render_er(Base, 'diagram.png')
+
+
+
+
